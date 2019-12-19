85f0fdb84a558b5a58e619e3a84468819fab9a1e
Pagliacii <huangmianrui0310@outlook.com>
2019-12-19 17:51:24 +0800

    feat(reverse-a-string/src/lib.rs): implement `reverse` function and add test case

--- reverse-a-string/src/lib.rs
+++ reverse-a-string/src/lib.rs
@@ -1,7 +1,22 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_reverse_ascii_string() {
+        let test = String::from("hello, world!");
+        let excepted = String::from("!dlrow ,olleh");
+        assert_eq!(reverse(test), excepted);
     }
+}
+
+pub fn reverse(source: String) -> String {
+    let len = source.len();
+    let mut chars = source.as_str().chars();
+    let mut result = String::with_capacity(len);
+
+    for _ in 0..len {
+        result.insert(0, chars.next().unwrap());
+    }
+    result
 }
