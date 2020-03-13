f032adab765ab8163c6ed7a7445c4924f19cdf49
Pagliacii <huangmianrui0310@outlook.com>
2020-03-13 23:53:13 +0800

    feat(wccopy): add some methods of Config and implement the Default trait

--- wccopy/src/lib.rs
+++ wccopy/src/lib.rs
@@ -6,11 +6,13 @@ mod tests {
     }
 }
 
+use std::default::Default;
+
 /// Controls the behaviors of this program.
 ///
 /// The default behavior is that prints the newline counts, word counts,
-/// and character counts to screen. Just like using the `--lines`,
-/// `--words`, and `--chars` flags.
+/// and byte counts to screen. Just like using the `--lines`,
+/// `--words`, and `--bytes` flags.
 ///
 /// Printing order: newline, word, character, byte, maximum line length
 pub struct Config {
@@ -26,4 +28,83 @@ pub struct Config {
     max_width: bool,
     // files to be count
     files: Vec<String>,
+}
+
+impl Config {
+    /// byte_counts setter
+    pub fn byte_toggle(&mut self, enabled: bool) {
+        self.byte_counts = enabled;
+    }
+
+    /// byte_counts getter
+    pub fn is_byte_enable(&self) -> bool {
+        self.byte_counts
+    }
+
+    /// char_counts setter
+    pub fn char_toggle(&mut self, enabled: bool) {
+        self.char_counts = enabled;
+    }
+
+    /// char_counts getter
+    pub fn is_char_enable(&self) -> bool {
+        self.char_counts
+    }
+
+    /// line_counts setter
+    pub fn line_toggle(&mut self, enabled: bool) {
+        self.line_counts = enabled;
+    }
+
+    /// line_counts getter
+    pub fn is_line_enable(&self) -> bool {
+        self.line_counts
+    }
+
+    /// word_counts setter
+    pub fn word_toggle(&mut self, enabled: bool) {
+        self.word_counts = enabled;
+    }
+
+    /// word_counts getter
+    pub fn is_word_enable(&self) -> bool {
+        self.word_counts
+    }
+
+    /// max_width setter
+    pub fn width_toggle(&mut self, enabled: bool) {
+        self.max_width = enabled;
+    }
+
+    /// max_width getter
+    pub fn is_width_enable(&self) -> bool {
+        self.max_width
+    }
+
+    /// Appends a file to be counted.
+    pub fn append_file(&mut self, filename: String) {
+        self.files.push(filename);
+    }
+
+    /// Gets all files to be counted.
+    pub fn files(&self) -> &Vec<String> {
+        &self.files
+    }
+}
+
+impl Default for Config {
+    /// Returns the default Config.
+    ///
+    /// Default Config enables to print newline, word, and byte counts
+    /// that reads from standard input.
+    fn default() -> Self {
+        Config {
+            byte_counts: true,
+            char_counts: false,
+            line_counts: true,
+            word_counts: true,
+            max_width: false,
+            files: vec![String::from("-")],
+        }
+    }
 }
